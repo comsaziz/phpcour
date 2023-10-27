512dd542ab198a639fe27475f4cc28b9c132579f
CsAZiz <comsaziz2@gmail.com>
2023-10-27 19:12:44 +0300

    init

---
+++ string.php
@@ -0,0 +1,14 @@
+<?php
+$string ="CSAZIZ";
+echo $string;
+
+echo "<br>";
+ //echo strtolower($string);
+// echo strtoupper($string);
+//echo strlen($string);
+//echo $string[5];
+//$string[3] ="i";
+//echo $string;
+ //echo str_replace("CS","Pro",$string);
+  echo substr($string,2 );
+?>
