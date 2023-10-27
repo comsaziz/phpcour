bae83e8d116af443a7599046966918a47cba78e1
CsAZiz <comsaziz2@gmail.com>
2023-10-28 00:25:00 +0300

    init

---
+++ numbers.php
@@ -0,0 +1,25 @@
+<?php
+//echo 20 / 10;
+//echo pow(5,3);
+//echo sqrt(25);
+
+//echo round(4,9)
+//echo floor(9.2);
+//echo ceil(9.2);
+
+//echo 10 % 3;
+
+//$num = 9;
+//$num/=9;
+//echo $num;
+
+//echo max(3,10);
+//echo min(3,10,5,8,7);
+
+
+
+
+
+
+
+?>
