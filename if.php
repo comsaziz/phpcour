502d798fa63e83cff1e127a7b5ab6685d0f63be1
CsAZiz <comsaziz2@gmail.com>
2023-10-29 17:40:57 +0300

    init

---
+++ if.php
@@ -0,0 +1,19 @@
+<?php
+$number1 = 0;
+$number2 = 2;
+if($number1 > $number2){
+echo "The number rise";
+} elseif($number1 < $number2){
+echo"The number less";
+
+}
+
+else{
+    echo"Equal";
+}
+
+
+echo $number1;
+echo $number2;
+
+?>
