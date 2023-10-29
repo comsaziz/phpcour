5b46caa679cfbcaae03923da998235e4988c1d76
CsAZiz <comsaziz2@gmail.com>
2023-10-29 16:32:24 +0300

    init

---
+++ forms.php
@@ -0,0 +1,15 @@
+<form action="forms.php" method="GET">
+Name: <input type="text" Name="fname">
+<br>
+Last: <input type ="text" Name= "lname">
+<br>
+<input type = "submit">
+</form>
+
+<?php
+
+
+
+echo $_REQUEST["f_name"];
+echo $_REQUEST["f_name"];
+?>
