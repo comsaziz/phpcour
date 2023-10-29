c517ffcf8acfd4364861c7b31846f107936594ca
CsAZiz <comsaziz2@gmail.com>
2023-10-29 20:38:16 +0300

    init

---
+++ fun.php
@@ -0,0 +1,32 @@
+<?php
+
+function add($x , $y){
+return $x + $y; 
+
+
+
+}
+
+
+ echo add(92,4);
+
+
+
+
+
+function hello(){
+   
+    $user = "Azi";
+    if($user === "Aziz"){
+        echo "HELLO" . $user;
+    } else{
+        echo "Bye" . $user;
+
+    }
+   
+}
+
+ hello();   
+
+
+?>
