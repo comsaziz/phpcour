6d13d450a4841d733d44873ca47706ab0d2f5b4f
CsAZiz <comsaziz2@gmail.com>
2023-10-29 16:48:13 +0300

    init

---
+++ caul.php
@@ -0,0 +1,25 @@
+
+<body>
+<div class="container">
+    <form action="caul.php" method="GET" class="form">
+        
+<div>
+    <label >Enter Number1:</label>
+    <input type="Number" id="num1" name="number1" class ="form-control">
+</div>
+
+<div>
+    <label >Enter Number2:</label>
+    <input type="Number" id="num2" name="number2" class ="form-control">
+</div>
+<br>
+<input type="submit" class = "btn btn-primary">
+
+</form>
+</div>
+
+
+
+
+    
+</body>
