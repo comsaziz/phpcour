ab95de022851420fb652c1f23841795fdb097e85
CsAZiz <comsaziz2@gmail.com>
2023-10-29 18:02:04 +0300

    init

--- if.php
+++ if.php
@@ -1,19 +1,54 @@
 <?php
-$number1 = 0;
-$number2 = 2;
+$username = "Aziz";
+$email = "azizo@gmail.com";
+
+if($username === "Aziz" &&
+ $email === "azizo@gmail.com"){
+ echo "Welcome Admin";
+
+
+}
+
+
+
+
+
+
+
+
+
+
+?>
+
+
+
+
+
+
+
+
+
+
+
+
+<?php
+/*$number1 = 28;
+$number2 = 10;
 if($number1 > $number2){
 echo "The number rise";
+
+if($number1 == 20 ){
+echo "The number is 20";
+
+}
+
 } elseif($number1 < $number2){
-echo"The number less";
+echo "The number less";
 
 }
 
 else{
-    echo"Equal";
+    echo "Equal";
 }
-
-
-echo $number1;
-echo $number2;
-
+*/
 ?>
